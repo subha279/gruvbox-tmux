b8ff36711fe440f569a8ce191ae2ebaba35c46e5
Subha <111702137+subha279@users.noreply.github.com>
2025-02-28 19:14:50 +0530

    Update themes.sh

--- src/themes.sh
+++ src/themes.sh
@@ -1,79 +1,79 @@
 #!/usr/bin/env bash
 
-SELECTED_THEME="$(tmux show-option -gv @tokyo-night-tmux_theme)"
-TRANSPARENT_THEME="$(tmux show-option -gv @tokyo-night-tmux_transparent)"
+SELECTED_THEME="$(tmux show-option -gv @gruvbox-tmux_theme)"
+TRANSPARENT_THEME="$(tmux show-option -gv @gruvbox-tmux_transparent)"
 
 case $SELECTED_THEME in
-"storm")
+"dark")
   declare -A THEME=(
-    ["background"]="#24283b"
-    ["foreground"]="#a9b1d6"
-    ["black"]="#414868"
-    ["blue"]="#7aa2f7"
-    ["cyan"]="#7dcfff"
-    ["green"]="#73daca"
-    ["magenta"]="#bb9af7"
-    ["red"]="#f7768e"
-    ["white"]="#a9b1d6"
-    ["yellow"]="#e0af68"
+    ["background"]="#282828"
+    ["foreground"]="#ebdbb2"
+    ["black"]="#3c3836"
+    ["blue"]="#458588"
+    ["cyan"]="#689d6a"
+    ["green"]="#98971a"
+    ["magenta"]="#b16286"
+    ["red"]="#cc241d"
+    ["white"]="#ebdbb2"
+    ["yellow"]="#d79921"
 
-    ["bblack"]="#414868"
-    ["bblue"]="#7aa2f7"
-    ["bcyan"]="#7dcfff"
-    ["bgreen"]="#41a6b5"
-    ["bmagenta"]="#bb9af7"
-    ["bred"]="#f7768e"
-    ["bwhite"]="#787c99"
-    ["byellow"]="#e0af68"
+    ["bblack"]="#504945"
+    ["bblue"]="#83a598"
+    ["bcyan"]="#8ec07c"
+    ["bgreen"]="#b8bb26"
+    ["bmagenta"]="#d3869b"
+    ["bred"]="#fb4934"
+    ["bwhite"]="#fbf1c7"
+    ["byellow"]="#fabd2f"
   )
   ;;
 
-"day")
+"light")
   declare -A THEME=(
-    ["background"]="#d5d6db"
-    ["foreground"]="#343b58"
-    ["black"]="#0f0f14"
-    ["blue"]="#34548a"
-    ["cyan"]="#0f4b6e"
-    ["green"]="#33635c"
-    ["magenta"]="#5a4a78"
-    ["red"]="#8c4351"
-    ["white"]="#343b58"
-    ["yellow"]="#8f5e15"
+    ["background"]="#fbf1c7"
+    ["foreground"]="#3c3836"
+    ["black"]="#282828"
+    ["blue"]="#076678"
+    ["cyan"]="#427b58"
+    ["green"]="#79740e"
+    ["magenta"]="#8f3f71"
+    ["red"]="#9d0006"
+    ["white"]="#3c3836"
+    ["yellow"]="#b57614"
 
-    ["bblack"]="#9699a3"
-    ["bblue"]="#34548a"
-    ["bcyan"]="#0f4b6e"
-    ["bgreen"]="#33635c"
-    ["bmagenta"]="#5a4a78"
-    ["bred"]="#8c4351"
-    ["bwhite"]="#343b58"
-    ["byellow"]="#8f5815"
+    ["bblack"]="#928374"
+    ["bblue"]="#458588"
+    ["bcyan"]="#689d6a"
+    ["bgreen"]="#98971a"
+    ["bmagenta"]="#b16286"
+    ["bred"]="#cc241d"
+    ["bwhite"]="#3c3836"
+    ["byellow"]="#d79921"
   )
   ;;
 
 *)
-  # Default to night theme
+  # Default to Gruvbox Dark
   declare -A THEME=(
-    ["background"]="#1A1B26"
-    ["foreground"]="#a9b1d6"
-    ["black"]="#414868"
-    ["blue"]="#7aa2f7"
-    ["cyan"]="#7dcfff"
-    ["green"]="#73daca"
-    ["magenta"]="#bb9af7"
-    ["red"]="#f7768e"
-    ["white"]="#c0caf5"
-    ["yellow"]="#e0af68"
+    ["background"]="#282828"
+    ["foreground"]="#ebdbb2"
+    ["black"]="#3c3836"
+    ["blue"]="#458588"
+    ["cyan"]="#689d6a"
+    ["green"]="#98971a"
+    ["magenta"]="#b16286"
+    ["red"]="#cc241d"
+    ["white"]="#ebdbb2"
+    ["yellow"]="#d79921"
 
-    ["bblack"]="#2A2F41"
-    ["bblue"]="#7aa2f7"
-    ["bcyan"]="#7dcfff"
-    ["bgreen"]="#41a6b5"
-    ["bmagenta"]="#bb9af7"
-    ["bred"]="#ff9e64"
-    ["bwhite"]="#787c99"
-    ["byellow"]="#e0af68"
+    ["bblack"]="#504945"
+    ["bblue"]="#83a598"
+    ["bcyan"]="#8ec07c"
+    ["bgreen"]="#b8bb26"
+    ["bmagenta"]="#d3869b"
+    ["bred"]="#fb4934"
+    ["bwhite"]="#fbf1c7"
+    ["byellow"]="#fabd2f"
   )
   ;;
 esac
@@ -83,9 +83,9 @@ if [ "${TRANSPARENT_THEME}" == 1 ]; then
   THEME["background"]="default"
 fi
 
-THEME['ghgreen']="#3fb950"
-THEME['ghmagenta']="#A371F7"
-THEME['ghred']="#d73a4a"
-THEME['ghyellow']="#d29922"
+THEME['ghgreen']="#b8bb26"
+THEME['ghmagenta']="#d3869b"
+THEME['ghred']="#fb4934"
+THEME['ghyellow']="#fabd2f"
 
 RESET="#[fg=${THEME[foreground]},bg=${THEME[background]},nobold,noitalics,nounderscore,nodim]"
